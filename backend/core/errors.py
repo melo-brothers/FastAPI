93147b4a325a1bee6f3f0f151d588a3edf5f8a9e
F2804179 Eli Junior <eli.junior@bb.com.br>
2022-12-28 14:21:13 -0300

    create errors

---
+++ backend/core/errors.py
@@ -0,0 +1,32 @@
+from collections import defaultdict
+
+error_messages = {
+    4001: {
+        "code": 4001,
+        "http_status": 400,
+        "type": "Alerta",
+        "category": "Negocial",
+        "name": "Parâmetro incorreto",
+        "description": "Verifique os dados da requisição",
+        "message": "Algum valor foi passado incorretamente verifique os campos do tipo data",
+        "action": "correct_data",
+
+    },
+}
+
+DEFAULT_MSG = {
+    "code": 5000,
+    "http_status": 500,
+    "type": "Alert",
+    "category": "System",
+    "subcategory": "Internal",
+    "name": "Erro ao processar",
+    "description": "Erro ao processar a operação",
+    "message": "Erro ao processar a operação",
+    "action": "Por favor registre uma...",
+}
+
+error_messages = defaultdict(lambda: DEFAULT_MSG, error_messages)
+
+
+def 
